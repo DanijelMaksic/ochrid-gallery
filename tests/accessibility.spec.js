8a4e5f08e6cf45c7b8b6314ec6cb35d312f41a14
DanijelMaksic <danijel.maksic77@gmail.com>
2026-04-02 12:18:53 +0200

    Added visual regression test for homepage

--- tests/accessibility.spec.js
+++ tests/accessibility.spec.js
@@ -1,10 +1,19 @@
 import { test, expect } from '@playwright/test';
 import AxeBuilder from '@axe-core/playwright';
 
-test('homepage has no accessibility violations', async ({ page }) => {
-   await page.goto('/');
+test.describe('homepage', () => {
+   test('has no accessibility violations', async ({ page }) => {
+      await page.goto('/');
 
-   const results = await new AxeBuilder({ page }).analyze();
+      const results = await new AxeBuilder({ page }).analyze();
 
-   expect(results.violations).toEqual([]);
+      expect(results.violations).toEqual([]);
+   });
+
+   test('looks correct', async ({ page }) => {
+      // Visual regression test
+      await page.goto('/');
+      await page.waitForSelector('main');
+      await expect(page).toHaveScreenshot();
+   });
 });
